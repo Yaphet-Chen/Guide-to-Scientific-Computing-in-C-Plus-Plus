bf462d752be50ac77166458460df88f26a21a355
Yaphet-Chen <619112510@qq.com>
2018-09-27 15:05:24 +0800

    my first c++ file, HelloWorld

---
+++ Chapter 1 - Getting Started/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    /* This is a comment and will be ignored by the compiler
+    Comments are useful to explain in English what
+    the program does */
+
+    // Print "Hello World" to the screen
+    std::cout << "Hello world\n";
+
+    return 0;
+}
