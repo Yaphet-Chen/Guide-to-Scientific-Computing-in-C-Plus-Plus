432ba677f066777051593900d4bf6983eba055a7
Yaphet-Chen <619112510@qq.com>
2018-09-28 20:58:06 +0800

    add ExceptionHandle

---
+++ Chapter 1 - Getting Started/ExceptionHandle.cpp
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------*\
+ * @Brief       : A less sophisticated exception handle - assert statements
+ * 
+ * @File        : ExceptionHandle.cpp
+ * @Author      : Yipei Chen
+ * @Email       : ychendh@connect.ust.hk
+ * @Date        : 2018-09-28
+\*---------------------------------------------------------------------------*/
+
+#include <iostream> //Note the inclusion of the extra header file cassert that is required to use assert statements.
+#include <cassert>
+#include <cmath>
+
+int main(int argc, char *argv[])
+{
+    double a;
+    std::cout << "Enter a non-negative number\n";
+    std::cin >> a;
+    
+    /* Another advantage of assert statements is that they are automatically removed when compiled
+    with the “-O” optimisation flag, so that you can test code with the assertions activated but
+    distribute a faster program that has them deactivated. */
+    assert(a >= 0.0);
+    std::cout << "The square root of " << a;
+    std::cout << " is " << sqrt(a) << "\n";
+    return 0;
+}
--- Chapter 1 - Getting Started/SimpleIO.cpp
+++ Chapter 1 - Getting Started/SimpleIO.cpp
@@ -1,7 +1,16 @@
+/*---------------------------------------------------------------------------*\
+ * @Brief       : Statement “std::cout.flush();” & String input
+ * 
+ * @File        : SimpleIO.cpp
+ * @Author      : Yipei Chen
+ * @Email       : ychendh@connect.ust.hk
+ * @Date        : 2018-09-28
+\*---------------------------------------------------------------------------*/
+
 #include <iostream>
 #include <string>
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
     int x = 1, y = 2;
 
@@ -10,7 +19,7 @@ int main(int argc, char * argv[])
     is printed before any other statements are executed, as shown in the listing below. */
     std::cout << "\'\t\a\" x = " << x << " and y = " << y << "\n"; //tab \t, bell sound \a
     std::cout.flush();
-    
+
     //std::cin may be used to ask for more than one input at a time, as shown below.
     int account_number, pin;
     std::cout << "Enter your account number\n";
@@ -21,6 +30,6 @@ int main(int argc, char * argv[])
     std::cout << "Enter your name and then hit RETURN\n";
     std::getline(std::cin, name); //Keyboard input for variables of type string is slightly different.
     std::cout << "Your name is " << name << "\n";
-    
+
     return 0;
 }
