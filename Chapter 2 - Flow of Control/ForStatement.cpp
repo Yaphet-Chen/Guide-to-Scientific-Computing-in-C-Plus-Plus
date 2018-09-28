92eaf930d5bb338cfe715e5a61931f86eec8e339
Yaphet-Chen <619112510@qq.com>
2018-09-28 23:52:59 +0800

    add ForStatement

---
+++ Chapter 2 - Flow of Control/ForStatement.cpp
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------*\
+ * @Brief       : For loops
+ * 
+ * @File        : ForStatement.cpp
+ * @Author      : Yipei Chen
+ * @Email       : ychendh@connect.ust.hk
+ * @Date        : 2018-09-28
+\*---------------------------------------------------------------------------*/
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < 10; i += 2)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+    std::cout.flush();
+
+    // * * * * * * * * * * * * * * Nest For loops * * * * * * * * * * * * * //
+    for (int i = 0; i < 5; i++) // Outer loop
+    {
+        for (int j = 5; j > i; j--) // Inner loop
+        {
+            std::cout << "i = " << i
+                      << " j = " << j << "\n";
+        }
+    }
+}
--- Chapter 2 - Flow of Control/IfElseStatement.cpp
+++ Chapter 2 - Flow of Control/IfElseStatement.cpp
@@ -8,7 +8,6 @@
 \*---------------------------------------------------------------------------*/
 
 #include <iostream>
-#include <string>
 
 int main(int argc, char *argv[])
 {
